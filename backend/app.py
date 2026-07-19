7347fa269a8246c45c19ef8978b622260a18671b
gowthamchowdary-k <2400030874@kluniversity.in>
2026-07-19 17:29:52 +0530

    Version 1.2:Adding memory to the chat and Added streaming responses

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,47 @@
 from chatbot import ask_ai
 
+messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are an AI Study Assistant. "
+            "Always respond in English "
+            "Give clear, accurate and concise answers."
+        )
+    }
+]
+
 print("=" * 40)
 print("🤖 AI Study Assistant")
 print("=" * 40)
 
-question = input("Ask: ")
+while True:
 
-answer = ask_ai(question)
+    question = input("\nYou: ")
 
-print("\nAI:")
-print(answer)
+    if question.lower() == "exit":
+        print("\n👋 Thank you for using AI Study Assistant!")
+        break
+
+    messages.append(
+        {
+            "role": "user",
+            "content": question
+        }
+    )
+
+    try:
+        answer = ask_ai(messages)
+
+        messages.append(
+            {
+                "role": "assistant",
+                "content": answer
+            }
+        )
+
+
+    except Exception as e:
+        print("\nAI:")
+        print("❌ Sorry, I couldn't contact the AI service.")
+        print(f"Error: {e}")
--- backend/chatbot.py
+++ backend/chatbot.py
@@ -6,15 +6,24 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
 )
 
-def ask_ai(question):
-    response = client.chat.completions.create(
+def ask_ai(messages):
+    stream = client.chat.completions.create(
         model="tencent/hy3:free",
-        messages=[
-            {
-                "role": "user",
-                "content": question
-            }
-        ]
+        messages=messages,
+        stream=True
     )
 
-    return response.choices[0].message.content
+    full_response = ""
+
+    print("\nAI: ", end="", flush=True)
+
+    for chunk in stream:
+        if chunk.choices[0].delta.content:
+            text = chunk.choices[0].delta.content
+            full_response += text
+
+            print(text, end="", flush=True)
+
+    print()
+
+    return full_response
